8d9a9d500020abbfdcd30693ce56e629e958e622
Christian Fenzl <fenzl@t-r-t.at>
2018-02-27 16:30:49 +0100

    Moved raspiBackup install to postroot

--- postroot.sh
+++ postroot.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # Will be executed as user "root".
 
-loxberryhome=$5
-pluginname=$3
-lbbackupconfig="/usr/local/etc/raspiBackup.conf"
 
 echo "<INFO> Installing raspiBackup"
 
 # Install raspiBackup
-chmod +x /tmp/uploads/$1/data/raspiBackupInstall.sh
-bash /tmp/uploads/$1/data/raspiBackupInstall.sh -c > /tmp/uploads/$1/log/raspiBackup.log 2>&1
-chmod a+w /tmp/uploads/$1/log/raspiBackup.log
+chmod +x ./raspiBackupInstall.sh
+bash ./raspiBackupInstall.sh -c > REPLACELBPLOGDIR/raspiBackup.log 2>&1
+chmod a+w REPLACELBPLOGDIR/raspiBackup.log
 
 # We have to default raspiBackup to not zip, as we cannot override this for rsync backups (will fail)
 #	sed -i.bak 's/^\(DEFAULT_ZIP_BACKUP=\).*/\1"0"/' $lbbackupconfig 
