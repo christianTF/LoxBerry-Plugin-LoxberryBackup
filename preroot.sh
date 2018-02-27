d490d984c089e43ba2b49790458e31da9a9f5809
Christian Fenzl <fenzl@t-r-t.at>
2018-02-27 12:19:45 +0100

    preroot.sh

---
+++ preroot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Will be executed as user "root".
+
+loxberryhome=$5
+pluginname=$3
+lbbackupconfig="/usr/local/etc/raspiBackup.conf"
+
+echo "<INFO> Installing raspiBackup"
+
+# Install raspiBackup
+bash /tmp/uploads/$1/data/raspiBackupInstall.sh -c > /tmp/uploads/$1/log/raspiBackup.log 2>&1
+chmod a+w /tmp/uploads/$1/log/raspiBackup.log
+# We have to default raspiBackup to not zip, as we cannot override this for rsync backups (will fail)
+#	sed -i.bak 's/^\(DEFAULT_ZIP_BACKUP=\).*/\1"0"/' $lbbackupconfig 
+
+# Create backup directory if missing
+if [ ! -d "/backup" ]; then
+	mkdir -p /backup
+	chown loxberry:loxberry /backup 
+fi
+
+exit 0
