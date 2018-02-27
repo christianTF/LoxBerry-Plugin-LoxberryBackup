7871b5d7d03011cd6b4c5ca79f77d75df9f1cbb0
Christian Fenzl <fenzl@t-r-t.at>
2018-02-27 12:35:55 +0100

    Deleted daemon

--- postupgrade.sh
+++ postupgrade.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-# Bash script which is executed in case of an update (if this plugin is already
-# installed on the system). This script is executed as very last step (*AFTER*
-# postinstall) and can be for example used to save back or convert saved
-# userfiles from /tmp back to the system. Use with caution and remember, that
-# all systems may be different! Better to do this in your own Pluginscript if
-# possible.
-#
-# Exit code must be 0 if executed successfull.
-#
-# Will be executed as user "loxberry".
-#
-# We add 5 arguments when executing the script:
-# command <TEMPFOLDER> <NAME> <FOLDER> <VERSION> <BASEFOLDER>
-#
-# For logging, print to STDOUT. You can use the following tags for showing
-# different colorized information during plugin installation:
-#
-# <OK> This was ok!"
-# <INFO> This is just for your information."
-# <WARNING> This is a warning!"
-# <ERROR> This is an error!"
-# <FAIL> This is a fail!"
-
 # To use important variables from command line use the following code:
 ARGV0=$0 # Zero argument is shell command
 # echo "<INFO> Command is: $ARGV0"
